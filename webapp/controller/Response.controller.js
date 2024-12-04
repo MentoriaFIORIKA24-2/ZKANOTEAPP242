0fedbf82b6d3da49ce8a204936dd9db84cb95f73
FIORI Teacher <danielle.campos@kasolution.com.br>
2024-12-04 21:31:21 +0000

    View response

---
+++ webapp/controller/Response.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+  ], (Controller, JSONModel) => {
+    "use strict";
+  
+    return Controller.extend("mentoria.fiori.ka.zkanoteapp242.controller.Response", {
+      onInit() {
+  
+      },
+  
+
+    });
+  });
